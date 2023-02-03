f229461be3cdf7b9a2244265a996c18707c94ba4
Robert Best <chessscholar@gmail.com>
2023-02-03 13:32:38 -0500

    try saving graph to file

--- tabs-tabs-tabs/tabs.js
+++ tabs-tabs-tabs/tabs.js
@@ -3,6 +3,52 @@ const ZOOM_INCREMENT = 0.2;
 const MAX_ZOOM = 5;
 const MIN_ZOOM = 0.3;
 const DEFAULT_ZOOM = 1;
+//import {Graph} from "https://wardcunningham.github.io/graph/graph.js"
+class Graph {
+  constructor(nodes=[], rels=[]) {
+    this.nodes = nodes;
+    this.rels = rels;
+  }
+
+  addNode(type, props={}){
+    const obj = {type, in:[], out:[], props};
+    this.nodes.push(obj);
+    return this.nodes.length-1;
+  }
+
+  addRel(type, from, to, props={}) {
+    const obj = {type, from, to, props};
+    this.rels.push(obj);
+    const rid = this.rels.length-1;
+    this.nodes[from].out.push(rid)
+    this.nodes[to].in.push(rid);
+    return rid;
+  }
+
+  tally(){
+    const tally = list => list.reduce((s,e)=>{s[e.type] = s[e.type] ? s[e.type]+1 : 1; return s}, {});
+    return { nodes:tally(this.nodes), rels:tally(this.rels)};
+  }
+
+  size(){
+    return this.nodes.length + this.rels.length;
+  }
+  stringify(...args) {
+    const obj = { nodes: this.nodes, rels: this.rels }
+    return JSON.stringify(obj, ...args)
+  }
+
+}
+
+function download(string, file, mime='text/json') {
+  var data = `data:${mime};charset=utf-8,` + encodeURIComponent(string)
+  var anchor = document.createElement('a')
+  anchor.setAttribute("href", data)
+  anchor.setAttribute("download", file)
+  document.body.appendChild(anchor) // required for firefox
+  anchor.click()
+  anchor.remove()
+}
 
 function firstUnpinnedTab(tabs) {
   for (let tab of tabs) {
@@ -167,10 +213,14 @@ document.addEventListener("click", (e) => {
 
   else if (e.target.id === "tabs-to-graph") {
     function logTabs(tabs) {
+      const graph = new Graph()
       for (const tab of tabs) {
         // tab.url requires the `tabs` permission or a matching host permission.
-        console.log(tab.url);
+        //console.log({...tab});
+        graph.addNode("Tab",{name:tab.title})
       }
+      console.log(graph,graph.stringify(null,2))
+      download(graph.stringify(null,2),"tabs.graph.json")
     }
     
     function onError(error) {
